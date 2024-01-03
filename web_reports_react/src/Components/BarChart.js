eec7ed56cb456e90ec9ac1504c99ec1e54cc019a
prakashhclSoftware <153637578+prakashhclSoftware@users.noreply.github.com>
2024-01-03 19:50:14 +0530

    Update Feed to add Dummy barChart

--- web_reports_react/src/Components/BarChart.js
+++ web_reports_react/src/Components/BarChart.js
@@ -1,19 +1,26 @@
+// BarChart.js
 import React from "react";
 import { Bar } from "react-chartjs-2";
-// import { Chart as ChartJS } from 'chart.js/auto'
+
 function BarChart() {
+  const chartStyle = {
+    width: '100%', 
+  };
+
   return (
-    <Bar
-      data={{
-        labels: ["A", "B", "C"],
-        datasets: [
-          {
-            label: "revenue",
-            data: [200, 300, 400],
-          },
-        ],
-      }}
-    />
+    <div style={chartStyle}>
+      <Bar
+        data={{
+          labels: ["A", "B", "C"],
+          datasets: [
+            {
+              label: "revenue",
+              data: [200, 300, 400],
+            },
+          ],
+        }}
+      />
+    </div>
   );
 }
 
--- web_reports_react/src/Components/Feed.js
+++ web_reports_react/src/Components/Feed.js
@@ -1,13 +1,32 @@
+import React from 'react';
 import { Box } from '@mui/material';
 import BarChart from './BarChart';
 
 const Feed = () => {
+  const boxStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    margin: 2,
+  };
+
+  const barChartContainerStyle = {
+    width: 400,
+  };
+
   return (
-    <Box sx={{ m: 2 }}>
-        {/* <BarChart /> Needs to be fixed */}
-        
+    <Box style={boxStyle}>
+      <div style={barChartContainerStyle}>
+        <BarChart />
+      </div>
+      <div style={barChartContainerStyle}>
+        <BarChart />
+      </div>
+      <div style={barChartContainerStyle}>
+        <BarChart />
+      </div>
     </Box>
   );
 };
 
-export default Feed
+export default Feed;
