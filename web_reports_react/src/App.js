089f1afe53d258d3c0a85ac6ca3802241d757f2c
nandishmajjagi17 <153698954+nandishmajjagi17@users.noreply.github.com>
2024-01-05 13:59:48 +0530

    Updated App.js

--- web_reports_react/src/App.js
+++ web_reports_react/src/App.js
@@ -1,12 +1,38 @@
 import Navbar from './Components/Navbar'
-import Feed from './Components/Feed'
+
+import React, { useReducer } from 'react'
 import Chart from 'chart.js/auto';
+import { useDispatch, useSelector, useStore } from 'react-redux'
+
+export const ACTIONS = {
+  SET_BAR_CHART_DATA: 'set_bar_chart_data',
+}
+
+const initialState = {
+  barChartData: []
+}
+
+
+export function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.SET_BAR_CHART_DATA:
+      return {
+        ...state,
+        barChartData: [1]     
+        // barChartData: action.payload.customeTabIndex
+      }
+    default:
+      return initialState
+  }
+}
 
 function App() {
+  
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <div className="App">
-     <Navbar />
-     <Feed />
+     <Navbar state={state}  dispatch={dispatch}/>
+   
     </div>
   );
 }
